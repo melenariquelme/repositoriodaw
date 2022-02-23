68c473b90230ea6373f54ed0df69c61bfb7a87bf
melenariquelme <100130445+melenariquelme@users.noreply.github.com>
2022-02-23 12:38:10 +0100

    Add files via upload

---
+++ EstuvoAqui/src/EstuvoAqui.java
@@ -0,0 +1,18 @@
+
+public class EstuvoAqui {
+
+	public static void main(String[] args) {
+
+		System.out.println("JM ya estuvo aquí \n");
+		
+		System.out.println("Jose Manuel ha estado hoy aqui \n");
+
+		System.out.println("Ismael ha estado hoy aqui \n");
+
+		System.out.println("JM ya estuvo aquí dos veces\n");
+
+		System.out.println("JM ya estuvo aquí tresveces\n");
+
+	}
+
+}
