3fdb10955bf0bf226bce4aa8907a0220d7478068
AlfonsoSanchezSe <1902897@alu.murciaeduca.es>
2025-03-11 13:24:01 +0100

    Arreglado fichero.php

--- fichero.php
+++ fichero.php
@@ -1,6 +1,6 @@
 <?php
 
-	echo "Esto es una linea de codigo para el repositorio de la practica 5 Parte 1"
+	echo "Esto es una linea de codigo para el repositorio de la practica 5 Parte 1";
 	echo "Alfonso estuvo aqui"
 
 <?
